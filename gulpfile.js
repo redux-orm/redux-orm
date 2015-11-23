c1074adeb5343bc3687063ff41a325c8782cd916
Tommi Kaikkonen <tommi.kaikkonen@aalto.fi>
2015-11-22 20:50:14 -0800

    Update 2015-11-23T04:50:11.684Z

--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-const gulp = require('gulp');
-const ghPages = require('gulp-gh-pages');
-
-gulp.task('deploy', () => {
-    return gulp.src('./dist/**/*')
-        .pipe(ghPages());
-});
--- src/constants.js
+++
@@ -1,4 +0,0 @@
-export const UPDATE = 'REDUX_ORM_UPDATE';
-export const DELETE = 'REDUX_ORM_DELETE';
-export const CREATE = 'REDUX_ORM_CREATE';
-export const ORDER = 'REDUX_ORM_ORDER';
