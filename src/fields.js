e16ee16c3dac9e4ce13224a759c9c02bc57a99f3
Tommi Kaikkonen <tommi.kaikkonen@aalto.fi>
2016-10-09 13:54:26 -0700

    Add documentation to fields.

--- src/fields.js
+++ src/fields.js
@@ -1,3 +1,7 @@
+/**
+ * @module fields
+ */
+
 const Field = class Field {
     constructor(...args) {
         if (args.length === 1 && typeof args[0] === 'object') {
@@ -16,14 +20,143 @@ export const ForeignKey = class ForeignKey extends Field {};
 export const ManyToMany = class ManyToMany extends Field {};
 export const OneToOne = class OneToOne extends Field {};
 
+/**
+ * Defines a foreign key on a model, which points
+ * to a single entity on another model.
+ *
+ * You can pass arguments as either a single object,
+ * or two arguments.
+ *
+ * If you pass two arguments, the first one is the name
+ * of the Model the foreign key is pointing to, and
+ * the second one is an optional related name, which will
+ * be used to access the Model the foreign key
+ * is being defined from, from the target Model.
+ *
+ * If the related name is not passed, it will be set as
+ * `${toModelName}Set`.
+ *
+ * If you pass an object to `fk`, it has to be in the form
+ *
+ * ```javascript
+ * fields = {
+ *   author: fk({ to: 'Author', relatedName: 'books' })
+ * }
+ * ```
+ *
+ * Which is equal to
+ *
+ * ```javascript
+ * fields = {
+ *   author: fk('Author', 'books'),
+ * }
+ * ```
+ *
+ * @param  {string|boolean} toModelNameOrObj - the `modelName` property of
+ *                                           the Model that is the target of the
+ *                                           foreign key, or an object with properties
+ *                                           `to` and optionally `relatedName`.
+ * @param {string} [relatedName] - if you didn't pass an object as the first argument,
+ *                                 this is the property name that will be used to
+ *                                 access a QuerySet the foreign key is defined from,
+ *                                 from the target model.
+ * @return {ForeignKey}
+ */
 export function fk(...args) {
     return new ForeignKey(...args);
 }
 
+/**
+ * Defines a many-to-many relationship between
+ * this (source) and another (target) model.
+ *
+ * The relationship is modeled with an extra model called the through model.
+ * The through model has foreign keys to both the source and target models.
+ *
+ * You can define your own through model if you want to associate more information
+ * to the relationship. A custom through model must have at least two foreign keys,
+ * one pointing to the source Model, and one pointing to the target Model.
+ *
+ * If you have more than one foreign key pointing to a source or target Model in the
+ * through Model, you must pass the option `throughFields`, which is an array of two
+ * strings, where the strings are the field names that identify the foreign keys to
+ * be used for the many-to-many relationship. Redux-ORM will figure out which field name
+ * points to which model by checking the through Model definition.
+ *
+ * Unlike `fk`, this function accepts only an object argument.
+ *
+ * ```javascript
+ * class Authorship extends Model {}
+ * Authorship.modelName = 'Authorship';
+ * Authorship.fields = {
+ *   author: fk('Author', 'authorships'),
+ *   book: fk('Book', 'authorships'),
+ * };
+ *
+ * class Author extends Model {}
+ * Author.modelName = 'Author';
+ * Author.fields = {
+ *   books: many({
+ *     to: 'Book',
+ *     relatedName: 'authors',
+ *     through: 'Authorship',
+ *
+ *     // this is optional, since Redux-ORM can figure
+ *     // out the through fields itself as there aren't
+ *     // multiple foreign keys pointing to the same models.
+ *     throughFields: ['author', 'book'],
+ *   })
+ * };
+ *
+ * class Book extends Model {}
+ * Book.modelName = 'Book';
+ * ```
+ *
+ * You should only define the many-to-many relationship on one side. In the
+ * above case of Authors to Books through Authorships, the relationship is
+ * defined only on the Author model.
+ *
+ * @param  {Object} options - options
+ * @param  {string} options.to - the `modelName` attribute of the target Model.
+ * @param  {string} [options.through] - the `modelName` attribute of the through Model which
+ *                                    must declare at least one foreign key to both source and
+ *                                    target Models. If not supplied, Redux-Orm will autogenerate
+ *                                    one.
+ * @param  {string[]} [options.throughFields] - this must be supplied only when a custom through
+ *                                            Model has more than one foreign key pointing to
+ *                                            either the source or target mode. In this case
+ *                                            Redux-ORM can't figure out the correct fields for
+ *                                            you, you must provide them. The supplied array should
+ *                                            have two elements that are the field names for the
+ *                                            through fields you want to declare the many-to-many
+ *                                            relationship with. The order doesn't matter;
+ *                                            Redux-ORM will figure out which field points to
+ *                                            the source Model and which to the target Model.
+ * @param  {string} [options.relatedName] - the attribute used to access a QuerySet
+ *                                          of source Models from target Model.
+ * @return {ManyToMany}
+ */
 export function many(...args) {
     return new ManyToMany(...args);
 }
 
+/**
+ * Defines a one-to-one relationship. In database terms, this is a foreign key with the
+ * added restriction that only one entity can point to single target entity.
+ *
+ * The arguments are the same as with `fk`. If `relatedName` is not supplied,
+ * the source model name in lowercase will be used. Note that with the one-to-one
+ * relationship, the `relatedName` should be in singular, not plural.
+ * @param  {string|boolean} toModelNameOrObj - the `modelName` property of
+ *                                           the Model that is the target of the
+ *                                           foreign key, or an object with properties
+ *                                           `to` and optionally `relatedName`.
+ * @param {string} [relatedName] - if you didn't pass an object as the first argument,
+ *                                 this is the property name that will be used to
+ *                                 access a Model the foreign key is defined from,
+ *                                 from the target Model.
+ * @return {OneToOne}
+ */
 export function oneToOne(...args) {
     return new OneToOne(...args);
 }
