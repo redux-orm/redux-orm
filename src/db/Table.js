ec9e9b635d069c2ee55fce0cb17c58a7444747aa
Andrew Hutchings <andrew.hutchings@gmail.com>
2017-06-27 08:47:32 -0400

    Order query clauses optimally so that idAttribute lookups are performed first and can make use of an index

--- src/db/Table.js
+++ src/db/Table.js
@@ -1,6 +1,7 @@
 import reject from 'lodash/reject';
 import filter from 'lodash/filter';
 import orderBy from 'lodash/orderBy';
+import sortBy from 'lodash/sortBy';
 import ops from 'immutable-ops';
 
 import { FILTER, EXCLUDE, ORDER_BY } from '../constants';
@@ -100,9 +101,29 @@ const Table = class Table {
     }
 
     query(branch, clauses) {
-        return clauses.reduce((rows, { type, payload }) => {
+        const optimizedClauses = sortBy(clauses, ({ type, payload }) => {
+            if (type === FILTER && payload.hasOwnProperty(this.idAttribute)) {
+                return 1;
+            }
+
+            if (type === FILTER || type === EXCLUDE) {
+                return 2;
+            }
+
+            return 3;
+        });
+
+        return optimizedClauses.reduce((rows, { type, payload }, index) => {
             switch (type) {
             case FILTER: {
+                if (index === 0 && payload.hasOwnProperty(this.idAttribute) && payload[this.idAttribute]) {
+                    // Payload specified a primary key; Since that is unique, we can directly
+                    // return that.
+                    const id = payload[this.idAttribute];
+                    return this.idExists(branch, id)
+                        ? [this.accessId(branch, payload[this.idAttribute])]
+                        : [];
+                }
                 return filter(rows, payload);
             }
             case EXCLUDE: {
