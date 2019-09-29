db1f15d22c9890d236e57a53bb4afa2d5ce40dfc
Andrew Hutchings <andrew.hutchings@gmail.com>
2019-09-29 15:21:15 -0400

    Use Symbol.for to create symbols in the global symbol registry (#290)
    Prior to this change, when using redux-orm across JavaScript contexts (such as iframes and secondary windows), each context will have a unique reference for of the symbol for a key. This was leading to failed symbol checks.
    
    One specific use case this change supports is as follows -
    1. Use redux-orm in a browser window to create ORM state.
    2. Create a selector using an ORM instance as a parameter In a secondary window with its own copy of redux-orm.
    3. Run that selector, passing in the state reference from the primary window.
    
    Without Symbol.for, the `isOrmState` check in memoize.js fails, resulting in the selector's result function receiving the ORM state instead of the session instance.

--- src/constants.js
+++ src/constants.js
@@ -9,9 +9,9 @@ export const ORDER_BY = 'REDUX_ORM_ORDER_BY';
 export const SUCCESS = 'SUCCESS';
 export const FAILURE = 'FAILURE';
 
-export const STATE_FLAG = Symbol('REDUX_ORM_STATE_FLAG');
+export const STATE_FLAG = Symbol.for('REDUX_ORM_STATE_FLAG');
 
-export const ALL_INSTANCES = Symbol('REDUX_ORM_ALL_INSTANCES');
+export const ALL_INSTANCES = Symbol.for('REDUX_ORM_ALL_INSTANCES');
 
 export const ID_ARG_KEY_SELECTOR = (state, idArg) => (
     (typeof idArg === 'undefined') ? ALL_INSTANCES : idArg
